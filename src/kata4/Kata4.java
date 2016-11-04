a5ca2e726639e0f52cec97e99b18bc13be97b1a1
usuario <usuario@10.22.143.65>
2016-11-04 11:43:38 +0000

    kata 4 (3) Final con grafica

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -2,13 +2,18 @@
 package kata4;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class Kata4 {
 
-    public static void main(String[] args) {
-        File file = new File("c:\\pub"); //cuidado aqui
-        print(file.listFiles(),"");
+    public static void main(String[] args) throws IOException {
+        String fileName = "e1.txt";
+        ArrayList<String> mailList = MailListReader.read(fileName);
+        Histogram<String> histogram = mailHistogramBuilder.build(mailList);
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
     }
     
     private static void print(File[] files,String indent){
